ec4ae3b735023a03ef1c479246be79c1620ea7dc
Youtao Xing <youtao.xing@icloud.com>
2019-02-21 17:24:56 +0800

    add abs

---
+++ abs.go
@@ -0,0 +1,11 @@
+// Package newmath is a Travis-CI example package.
+package newmath
+
+// Abs returns the absolute value of x.
+//
+// Special cases are:
+//	Abs(±Inf) = +Inf
+//	Abs(NaN) = NaN
+func Abs(x float64) float64 {
+	return Float64frombits(Float64bits(x) &^ (1 << 63))
+}
